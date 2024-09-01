b2a6ea13904e4e25a9a395884565ba3223b9e8b5
FrancoPalavicinoG <fppalavicino@miuandes.cl>
2024-08-31 22:58:01 -0400

    diagramatics examples

--- staticfy_app/config/routes.rb
+++ staticfy_app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'static_pages/index'
+  get 'templates/example1'
+  get 'templates/example2'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
