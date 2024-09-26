065452c6c35b836140814df8ec211e396afa1e67
PedroVillegasss <ppvillegas@miuandes.cl>
2024-09-26 04:26:15 -0300

    arreglando errores criticos del franco y del pirela

--- staticfy_app/app/controllers/tasks_controller.rb
+++ staticfy_app/app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-    before_action :set_task
+    before_action :set_task, only: %i[show edit update destroy submit_answer]
     before_action :authenticate_user!
 
     # GET /tasks
